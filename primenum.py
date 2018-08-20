e3c598f55daaf5029df1a6dc09c13135572296a4
ReddyBrahmanandaReddy <41770140+ReddyBrahmanandaReddy@users.noreply.github.com>
2018-08-20 10:59:17 +0530

    Create primenum.py

---
+++ primenum.py
@@ -0,0 +1,10 @@
+a=2
+b=10
+print("")
+for num in range(a+1,b):
+   if num>1:
+       for i in range(2,num):
+           if(num % i)==0:
+               break
+       else:
+           print(num,end=" ")
